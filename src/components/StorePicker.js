5131744a97af9b115fbfbccaefb7177247ed468c
darragh169 <darragh169@gmail.com>
2016-10-18 21:39:01 -0700

    Added goToStore

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { getFunName} from '../helpers.js'
 
 class StorePicker extends React.Component {
+	
+	goToStore(event){
+		event.preventDefault();
+		console.log(this.storeInput.value);
+	}
+
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 				<h2>Please Enter a store</h2>
-				<input type="text" required placeholder= { getFunName() }  />
+				<input type="text" required placeholder={ getFunName() } defaultValue={getFunName()} ref={ (input) => { this.storeInput = input }} />
 				<button type="submit">Visit Store</button>
 			</form>
 		);
